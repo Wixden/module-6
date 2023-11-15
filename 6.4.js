2552d22ebc6e03edec384006eab5b119c52e6333
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-11-16 01:42:17 +0600

    More about  aggregation operators

--- 6.4.js
+++ 6.4.js
@@ -2,4 +2,19 @@
 
 // explore more about $group & $project
 
+// $group --> it will create a group/doc with the specified field: example: {"gender" : "Male"}
+/* 
+In this session we will learn more about the aggregation operators that work with $group.
+
+** $count
+** $max
+** $min
+** $avg
+** $sum
+** $push
+
+
+
+*/
+
 // =============================================================
